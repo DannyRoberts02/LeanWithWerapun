a6aadfd4d9d69427b43648d23673ffc4b7261432
DannyRoberts02 <robertsza69@gmail.com>
2024-03-20 17:58:00 +0700

    Version 3 -Add data type

---
+++ 2_data.type.ts
@@ -0,0 +1,30 @@
+// Primitive type (basic types):number , string ,bool
+//Math operators: + - * /
+//Comparison operators : > < = > = <=
+
+
+//Number
+console.log(100+1)
+console.log(-1)
+console.log(0.345)
+console.log(20 + 5 + 0 + 5 )
+
+
+//String 
+console.log("Hello World")
+console.log("1")
+console.log("19+30")
+console.log('single quote')
+console.log("Double quote")
+console.log(`Back quote`)
+console.log ("Message1"+"Message2"+"Message3")
+console.log("100"+"5"+"6"+"4")
+console.log(100+"5")
+
+//Boolean
+console.log(true)
+console.log(false)
+console.log(1>0)
+console.log(1<0)
+console.log( 1 === "1")
+console.log( 1 === "1")
